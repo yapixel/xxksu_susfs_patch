29d84a73b7b4050eba0a00de06077af83fe0fbd2
debiansid <9624961+debiansid@users.noreply.github.com>
2026-09-23 21:57:35 +0800

    checkpoint: establish authoritative GKI 6.12 source bundle

--- .github/scripts/v2/manifests/defaults.py
+++ .github/scripts/v2/manifests/defaults.py
@@ -11,7 +11,7 @@ _TARGET_DATA = {
     "gki-android16-6.12": {
         "kernel": ("https://android.googlesource.com/kernel/common", "android16-6.12"),
         "susfs50": ("https://gitlab.com/simonpunk/susfs4ksu", "gki-android16-6.12"),
-        "susfs50_commit": "698aa6a4ddca6fa5359871daf13f93583fb8282a",
+        "susfs50_commit": "c8f64e41e3dea2cd44754d7472d3cd0bc0b40784",
     },
     "sultan-android14-6.1": {
         "kernel": ("https://github.com/kerneltoast/android_kernel_google_tensynos", "16.0.0-sultan"),
